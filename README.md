c20661e2777da41655a57ab84eac9d905a8d04d8
chrisdaaz <chrisdaaz@users.noreply.github.com>
2022-05-02 15:54:00 +0000

    deploy: 3ff81c624d7d401d6d7bd47927ab173e0373a24e

--- README.md
+++
@@ -1,3 +0,0 @@
-# Samvera Community Website
-
-This repository was created by the [Samvera Website Migration Working Group](https://samvera.atlassian.net/wiki/spaces/samvera/pages/1935966209/Samvera+Website+Migration+Working+Group).
---
+++ _next/static/a9ZgcqZeuLfknyTk6mh72/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-ab15d1b50855c404.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
