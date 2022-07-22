4848c6344947cce32e2c75e234f7a08f69112909
chrisdaaz <chris-diaz@northwestern.edu>
2022-07-22 16:07:06 +0000

    Deploying to gh-pages from @ samvera-labs/samvera.org@645adf517311a5bb2e3c3a7640c87e46406a69e3 🚀

---
+++ _next/static/5XryxLWWsZ7P-KNjQMLzp/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fnews","\u002Fabout\u002F[slug]","\u002Fhome\u002F[slug]","\u002Fnews\u002F[slug]","\u002Fwhat-is-samvera\u002F[slug]","\u002Fwho-uses-samvera\u002F[slug]","\u002Fwhy-use-samvera\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
