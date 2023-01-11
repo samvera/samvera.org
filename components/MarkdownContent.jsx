cca1f3c844f1bd0fda0a38b68a5ae974e063b6c5
Adam J. Arling <aarling@gmail.com>
2023-01-11 11:31:57 -0600

    Handle parsing of HTML elements embedded within Markdown files

--- components/MarkdownContent.jsx
+++ components/MarkdownContent.jsx
@@ -1,6 +1,48 @@
+/**
+ * Good reference for how to parse markdown w/ HTML
+ * https://unifiedjs.com/learn/recipe/remark-html/
+ */
+
 import React from "react";
 import md from "markdown-it";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
+import deepmerge from "deepmerge";
+import rehypeRaw from "rehype-raw";
 
 export default function MarkdownContent({ content }) {
-  return <div dangerouslySetInnerHTML={{ __html: md().render(content) }}></div>;
+  const [processedContent, setProcessedContent] = React.useState();
+
+  React.useEffect(() => {
+    if (!content) return;
+
+    /**
+     * This is needed to parse <iframe /> HTML elements in markdown files
+     */
+    const newSchema = deepmerge(defaultSchema, {
+      attributes: { iframe: ["src"] },
+      tagNames: ["iframe"],
+    });
+
+    unified()
+      .use(remarkParse)
+      .use(remarkRehype, { allowDangerousHtml: true })
+      .use(rehypeRaw)
+      .use(rehypeSanitize, newSchema)
+      .use(rehypeStringify, { allowDangerousHtml: true })
+      .process(content)
+      .then((file) => {
+        setProcessedContent(String(file));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, [content]);
+
+  return <div dangerouslySetInnerHTML={{ __html: processedContent }}></div>;
 }
