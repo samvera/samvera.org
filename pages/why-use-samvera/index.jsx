d3f4c7377d4afc51f130f2ff82b19a3422316d87
Adam J. Arling <aarling@gmail.com>
2023-07-06 15:01:36 -0500

    Consolidate Why Use Samvera page into one page

--- pages/why-use-samvera/index.jsx
+++ pages/why-use-samvera/index.jsx
@@ -1,18 +1,7 @@
-import { getMarkdownPageContent, getPaths } from "lib/markdown-helpers";
-
 import DynamicPage from "components/layout/DynamicPage";
 import { buildWorkOpenGraphData } from "lib/open-graph";
+import { getMarkdownPageContent } from "lib/markdown-helpers";
 
-/**
- * This is where the magic happens.
- * 'parentDir' is the value of this file's parent directory
- * along with a matching directory title which exists in
- * /markdown.
- *
- * For example:
- * /markdown/my-cool-directory/...
- * /pages/my-cool-directory/...
- */
 const CONFIG = {
   parentDir: "why-use-samvera",
   parentDirLabel: "Why use Samvera?",
@@ -24,23 +13,15 @@ export default function WhatIsSamveraPage({ content, frontmatter }) {
   );
 }
 
-export async function getStaticPaths() {
-  const paths = getPaths(`markdown/${CONFIG.parentDir}`);
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params: { slug } }) {
+export async function getStaticProps() {
   const { content, frontmatter } = getMarkdownPageContent(
-    `markdown/${CONFIG.parentDir}/${slug}.md`
+    `markdown/${CONFIG.parentDir}/index.md`
   );
 
   const openGraphData = buildWorkOpenGraphData(
     CONFIG.parentDirLabel,
     frontmatter.title,
-    `${CONFIG.parentDir}/${slug}`
+    `${CONFIG.parentDir}/index`
   );
 
   return {
