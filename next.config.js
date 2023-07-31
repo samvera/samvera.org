1b5778b2698ee8f8d39fa9bd0f6036facb99f15b
Adam Joseph Arling <aarling@gmail.com>
2023-07-31 14:13:45 -0500

    Disable NextJS Image optimization since we're using a static site build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  experimental: { esmExternals: true },
+  experimental: {
+    esmExternals: true,
+  },
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
